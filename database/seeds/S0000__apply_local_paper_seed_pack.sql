aad429127ce30cdf2716c0f2f28a5e81c82abfc2
ranjithr007 <44235764+ranjithr007@users.noreply.github.com>
2026-06-29 19:49:40 +0530

    fix: isolate included seed scripts into separate batches

--- database/seeds/S0000__apply_local_paper_seed_pack.sql
+++ database/seeds/S0000__apply_local_paper_seed_pack.sql
@@ -11,8 +11,14 @@ SET NUMERIC_ROUNDABORT OFF;
 GO
 
 :r $(SeedRoot)\reference\S0001__seed_nse_exchange_and_draft_calendar.sql
+GO
 :r $(SeedRoot)\reference\S0002__seed_index_context.sql
+GO
 :r $(SeedRoot)\reference\S0003__seed_simulator_broker.sql
+GO
 :r $(SeedRoot)\policies\S0004__seed_paper_risk_policy.sql
+GO
 :r $(SeedRoot)\policies\S0005__seed_paper_order_transition_policy.sql
+GO
 :r $(VerificationRoot)\S0001__verify_reference_seeds.sql
+GO

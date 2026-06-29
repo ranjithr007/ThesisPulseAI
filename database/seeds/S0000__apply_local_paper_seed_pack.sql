828deb742a60b212e6c12a3ee7f528616b944442
ranjithr007 <44235764+ranjithr007@users.noreply.github.com>
2026-06-29 19:32:09 +0530

    seeds: add authoritative local paper seed pack

---
+++ database/seeds/S0000__apply_local_paper_seed_pack.sql
@@ -0,0 +1,18 @@
+:setvar SeedRoot ".\database\seeds"
+:setvar VerificationRoot ".\database\verification"
+
+SET ANSI_NULLS ON;
+SET QUOTED_IDENTIFIER ON;
+SET ANSI_PADDING ON;
+SET ANSI_WARNINGS ON;
+SET ARITHABORT ON;
+SET CONCAT_NULL_YIELDS_NULL ON;
+SET NUMERIC_ROUNDABORT OFF;
+GO
+
+:r $(SeedRoot)\reference\S0001__seed_nse_exchange_and_draft_calendar.sql
+:r $(SeedRoot)\reference\S0002__seed_index_context.sql
+:r $(SeedRoot)\reference\S0003__seed_simulator_broker.sql
+:r $(SeedRoot)\policies\S0004__seed_paper_risk_policy.sql
+:r $(SeedRoot)\policies\S0005__seed_paper_order_transition_policy.sql
+:r $(VerificationRoot)\S0001__verify_reference_seeds.sql

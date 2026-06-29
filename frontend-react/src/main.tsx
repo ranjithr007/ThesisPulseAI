a4a7bc0c1911841a85250e5adfd69fc7756f3661
ranjithr007 <44235764+ranjithr007@users.noreply.github.com>
2026-06-29 22:27:42 +0530

    style: load signal scanner stylesheet

--- frontend-react/src/main.tsx
+++ frontend-react/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import { App } from "./App";
 import "./styles.css";
+import "./signals/signal-scanner.css";
 
 const rootElement = document.getElementById("root");
 

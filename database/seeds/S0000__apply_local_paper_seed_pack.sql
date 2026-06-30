0ddee9db1ec0258b4f9c34574046a24836e99335
ranjithr007 <44235764+ranjithr007@users.noreply.github.com>
2026-06-30 09:32:12 +0530

    seed: apply and verify Feature Factory engine

--- database/seeds/S0000__apply_local_paper_seed_pack.sql
+++ database/seeds/S0000__apply_local_paper_seed_pack.sql
@@ -24,9 +24,13 @@ GO
 GO
 :r $(SeedRoot)/reference/S0007__seed_upstox_market_data_sources.sql
 GO
+:r $(SeedRoot)/intelligence/S0008__seed_feature_factory_engine.sql
+GO
 :r $(VerificationRoot)/S0001__verify_reference_seeds.sql
 GO
 :r $(VerificationRoot)/S0002__verify_signal_engine_seed.sql
 GO
 :r $(VerificationRoot)/S0003__verify_upstox_market_data_sources.sql
+GO
+:r $(VerificationRoot)/S0004__verify_feature_factory_seed.sql
 GO
